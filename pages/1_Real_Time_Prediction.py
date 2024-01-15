91886de354279edbd9eb1b41ab1a7d7962edee84
RosmeJ <rosmejoseph,07@gmail.com>
2024-01-15 14:17:58 +0530

    Removed unncessary changes

--- pages/1_Real_Time_Prediction.py
+++ pages/1_Real_Time_Prediction.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 from Home import face_rec
 from streamlit_webrtc import webrtc_streamer
@@ -42,54 +41,3 @@ def video_frame_callback(frame):
 webrctc_ctxt = webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback, rtc_configuration={
         "iceServers": [{"urls": ["stun:16.171.132.146:5349","turn:16.171.132.146:5349"]}]
     })
-=======
-import streamlit as st
-from Home import face_rec
-from streamlit_webrtc import webrtc_streamer
-import av
-import time
-import os
-from twilio.rest import Client
-#st.set_page_config(page_title='Predictions')
-
-st.subheader('Punch In')
-#Retrieve data from Redis Database
-with st.spinner('Retrieving Data from Redis DB...'):
-    redis_face_db = face_rec.retrieve_data(name='ArcEmployees:register')
-    st.dataframe(redis_face_db)
-st.success("Data successfully retrieved from Redis")
-# time
-waitTime= 10 # time in sec
-setTime = time.time()
-realtimepred = face_rec.RealTimePred() # Real Time Prediction class
-# Real Time Prediction
-#streamlit webrtc
-account_sid = os.environ['AC22fad8fc98762c8a961730377698f54b']
-auth_token = os.environ['5491572a11fa6ff2c945bdfd175d62b2']
-client = Client(account_sid, auth_token)
-
-token = client.tokens.create()
-# callback function
-difftime=0
-def video_frame_callback(frame):
-    global setTime
-    img = frame.to_ndarray(format="bgr24") #3d numpy array
-    #operation that can perform on the array
-    pred_img,person_name= realtimepred.face_prediction(img,redis_face_db,'facial_features',['Name','Role'],thresh=0.5)
-    timenow =time.time()
-    difftime = timenow - setTime
-    if difftime >= waitTime:
-        if(person_name != 'Unknown'):
-            st.success(f'{person_name} Logged in successfully')
-            realtimepred.saveLogs_redis()
-            setTime = time.time() # reset time
-            print(person_name)
-            print('save data to redis db') 
-          
-           
-    return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
-
-webrctc_ctxt = webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback, rtc_configuration={
-        "iceServers": token.ice_servers
-    })
->>>>>>> 2ce46fe9e23a8c689194b0cc268337b77cc76a63
--- pages/2_Registration_Form.py
+++ pages/2_Registration_Form.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 from Home import face_rec
 import cv2
@@ -42,52 +41,4 @@ if st.button('submit'):
     elif return_value == 'name_false':
         st.error('please enter the name : Name cannot be empty or spaces')
     elif return_value == 'file_false':
-=======
-import streamlit as st
-from Home import face_rec
-import cv2
-import numpy as np
-from streamlit_webrtc import webrtc_streamer
-import av
-import os
-from twilio.rest import Client
-#st.set_page_config(page_title='Registration Form')
-st.subheader('Registration Form')
-
-# init registration form
-registration_form = face_rec.RegistrationForm()
-# Step-1: collect person name and role
-# form 
-person_name = st.text_input(label='Name',placeholder='First & Last Name')
-Emp_Id = st.text_input(label='Emp Id',placeholder='ARC@12344')
-role= st.selectbox(label='select your Role',options=('SuperAdmin','Supervisor','User','Manager','HR'))
-account_sid = os.environ['AC22fad8fc98762c8a961730377698f54b']
-auth_token = os.environ['5491572a11fa6ff2c945bdfd175d62b2']
-client = Client(account_sid, auth_token)
-token = client.tokens.create()
-#step-2: Collect Facial embedding of the person
-
-def video_callback_func(frame):
-    img=frame.to_ndarray(format='bgr24') #3d array bgr
-    reg_image,embedding = registration_form.get_embedding(img)
-    # two step process
-    #1st step save data into local computer txt
-    if embedding is not None:
-        with open('face_embedding.txt',mode='ab') as f:
-            np.savetxt(f,embedding)
-    return av.VideoFrame.from_ndarray(reg_image,format='bgr24')
-
-webrtc_streamer(key="registration", video_frame_callback=video_callback_func, rtc_configuration={
-         "iceServers": token.ice_servers
-    })
-#step-3: save the data in redis Database
-
-if st.button('submit'):
-    return_value = registration_form.save_data_in_redis_db(person_name,role,Emp_Id)
-    if return_value == True:
-        st.success(f"{person_name} registered successfully")
-    elif return_value == 'name_false':
-        st.error('please enter the name : Name cannot be empty or spaces')
-    elif return_value == 'file_false':
->>>>>>> 2ce46fe9e23a8c689194b0cc268337b77cc76a63
         st.error('face_embedding.txt is not found. Please refresh the page and execute again')
